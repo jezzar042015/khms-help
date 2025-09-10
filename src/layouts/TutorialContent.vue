fc10dfcc3eed9fb64548c22ef810f2d304c17672
Jezreel Tan <jezdtanslc90@gmail.com>
2025-09-11 07:49:06 +0800

    fix lg screens on w

--- src/layouts/TutorialContent.vue
+++ src/layouts/TutorialContent.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="py-12 px-3">
+    <div class="pt-12 px-3 xl:px-96">
         <TutorialHeader :title="title" @to-tutorials="emits('off-tutorial')" />
 
         <slot name="default">
 
         </slot>
 
-        <div @click="emits('off-tutorial')" class="text-blue-500 cursor-pointer mt-5 py-3 px-3 underline">
+        <div @click="emits('off-tutorial')" class="text-blue-500 cursor-pointer mt-5 pt-3 pb-16 px-3 underline">
             Back To Tutorials
         </div>
     </div>
